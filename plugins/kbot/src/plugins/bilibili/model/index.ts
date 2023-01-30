f765c16db0d509674113b92ed971ac3a26129627
Kabuda-czh <634469564@qq.com>
2023-01-30 18:06:10 +0800

    🐳 chore: add model dir

---
+++ plugins/kbot/src/plugins/bilibili/model/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: Kabuda-czh
+ * @Date: 2023-01-30 17:18:31
+ * @LastEditors: Kabuda-czh
+ * @LastEditTime: 2023-01-30 17:21:13
+ * @FilePath: \KBot-App\plugins\kbot\src\plugins\bilibili\model\index.ts
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by ${git_name}, All Rights Reserved.
+ */
+export * from "./dynamic.model";
+export * from "./userInfo.model"
