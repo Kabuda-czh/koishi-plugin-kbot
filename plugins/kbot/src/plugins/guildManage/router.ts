e487c6a314a2856c49b76aed18869330661b0294
Kabuda-czh <634469564@qq.com>
2023-01-31 19:14:20 +0800

    🦄 refactor: Integration method

--- plugins/kbot/src/plugins/guildManage/router.ts
+++ plugins/kbot/src/plugins/guildManage/router.ts
@@ -2,14 +2,14 @@
  * @Author: Kabuda-czh
  * @Date: 2023-01-31 17:23:44
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-01-31 18:47:49
+ * @LastEditTime: 2023-01-31 19:12:47
  * @FilePath: \KBot-App\plugins\kbot\src\plugins\guildManage\router.ts
  * @Description:
  *
  * Copyright (c) 2023 by Kabuda-czh, All Rights Reserved.
  */
-import { Context, Universal } from "koishi";
-import { Context as KoaContext } from 'koa'
+import { Context, Universal, Bot } from "koishi";
+import { Context as KoaContext } from "koa";
 interface UserInfo {
   userId: string;
   username: string;
@@ -24,63 +24,33 @@ interface IRouterStrategy {
   [key: string]: (context: Context) => (ctx: KoaContext) => Promise<void>;
 }
 
+function handleFunction<T = any>(
+  context: Context,
+  functionName: keyof Bot,
+  ...args: any
+) {
+  return async (ctx) => {
+    await Promise.all(
+      context.bots.flatMap((bot) =>
+        bot.platform === "onebot" ? bot?.[functionName](...args.map(arg => arg = ctx.query?.[arg] as string || "")) : []
+      )
+    ).then((res: T[]) => {
+      ctx.body = res.flat();
+    });
+  };
+}
+
 export const routerStrategies: IRouterStrategy = {
   "/guildList": function (context: Context) {
-    return async (ctx) => {
-      await Promise.all(
-        context.bots.flatMap((bot) =>
-          bot.platform === "onebot" ? bot.getGuildList() : []
-        )
-      ).then((res: Universal.Guild[]) => {
-        ctx.body = res.flat();
-      });
-    };
+    return handleFunction<Universal.Guild>(context, "getGuildList")
   },
   "/guildMemberList": function (context: Context) {
-    return async (ctx) => {
-      await Promise.all(
-        context.bots.flatMap((bot) =>
-          bot.platform === "onebot"
-            ? bot.getGuildMemberList(ctx.query.guildId as string)
-            : []
-        )
-      ).then((res: GuildMemberInfo[]) => {
-        ctx.body = res.flat();
-      });
-    };
+    return handleFunction<GuildMemberInfo>(context, "getGuildMemberList", "guildId")
   },
   "/muteGuild": function (context: Context) {
-    return async (ctx) => {
-      await Promise.all(
-        context.bots.flatMap((bot) =>
-          bot.platform === "onebot"
-            ? bot.muteChannel(
-                ctx.query.guildId as string,
-                "",
-                (ctx.query.mute as string) === "1"
-              )
-            : []
-        )
-      ).then((res) => {
-        ctx.body = res.flat();
-      });
-    };
+    return handleFunction(context, "muteChannel", "guildId", "", "mute")
   },
   "/muteMember": function (context: Context) {
-    return async (ctx) => {
-      await Promise.all(
-        context.bots.flatMap((bot) =>
-          bot.platform === "onebot"
-            ? bot.muteGuildMember(
-                ctx.query.guildId as string,
-                ctx.query.userId as string,
-                1000 * 60
-              )
-            : []
-        )
-      ).then((res) => {
-        ctx.body = res.flat();
-      });
-    };
+    return handleFunction(context, "muteGuildMember", "guildId", "userId", "duration")
   },
 };
