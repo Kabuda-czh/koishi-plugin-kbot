8d0a1c1efb6ee4d578e42e6fcfef81d4ef543b7b
Kabuda-czh <634469564@qq.com>
2023-02-08 01:53:07 +0800

    ✨ feat: add auto set superAdmin auth

--- plugins/kbot/src/basic/index.ts
+++ plugins/kbot/src/basic/index.ts
@@ -2,13 +2,13 @@
  * @Author: Kabuda-czh
  * @Date: 2023-01-29 14:28:53
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-02-07 15:36:15
+ * @LastEditTime: 2023-02-08 01:52:19
  * @FilePath: \KBot-App\plugins\kbot\src\basic\index.ts
  * @Description:
  *
  * Copyright (c) 2023 by Kabuda-czh, All Rights Reserved.
  */
-import { Context, Schema } from "koishi";
+import { Context, Logger, Schema } from "koishi";
 
 export interface Config {
   superAdminQQ: string;
@@ -18,18 +18,34 @@ export const Config: Schema<Config> = Schema.object({
   superAdminQQ: Schema.string().required().description("超级管理员QQ号 (必填)"),
 });
 
-export const using = ['database']
+
+export const logger = new Logger("KBot-basic");
+
+export const using = ["database"];
 
 export function apply(ctx: Context, config: Config) {
-  ctx.database.getUser("onebot", config.superAdminQQ).then((user) => {
-    if (user) {
-      ctx.database.setUser("onebot", config.superAdminQQ, {
-        authority: 5,
-      });
-    } else {
-      ctx.database.createUser("onebot", config.superAdminQQ, {
-        authority: 5,
-      });
+  ctx.on("bot-status-updated", (bot) => {
+    if (bot.status === "online") {
+      logger.success("KBot已上线")
+      ctx.database
+        .getUser(bot.platform, config.superAdminQQ)
+        .then((user) => {
+          try {
+            if (user && user?.authority < 5) {
+              ctx.database.setUser(bot.platform, config.superAdminQQ, {
+                authority: 5,
+              });
+              logger.success(`已将QQ号为 ${config.superAdminQQ} 的用户权限设置为 5 级`)
+            } else {
+              ctx.database.createUser(bot.platform, config.superAdminQQ, {
+                authority: 5,
+              });
+              logger.success(`已成功创建QQ号为 ${config.superAdminQQ} 的用户, 并赋予权限 5 级`)
+            }
+          } catch (err) {
+            logger.error(`设置QQ号为 ${config.superAdminQQ} 的用户权限时出错: ${err}`)
+          }
+        });
     }
-  })
+  });
 }
