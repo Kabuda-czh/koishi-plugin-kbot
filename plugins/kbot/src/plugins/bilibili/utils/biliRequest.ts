9c14bdcf2b3dc9e4bff1045f7c08aa0968da12de
Kabuda-czh <634469564@qq.com>
2023-02-09 18:25:22 +0800

    🦄 refactor: review code & finish request

--- plugins/kbot/src/plugins/bilibili/utils/biliRequest.ts
+++ plugins/kbot/src/plugins/bilibili/utils/biliRequest.ts
@@ -2,7 +2,7 @@
  * @Author: Kabuda-czh
  * @Date: 2023-02-03 16:34:11
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-02-08 14:31:05
+ * @LastEditTime: 2023-02-09 17:13:08
  * @FilePath: \KBot-App\plugins\kbot\src\plugins\bilibili\utils\biliRequest.ts
  * @Description:
  *
@@ -10,6 +10,7 @@
  */
 import { Quester } from "koishi";
 import { BilibiliDynamicType } from "../enum";
+import { MemberCard, MedalWall } from "../model";
 import { StringFormat } from "./format";
 
 export async function getDynamic(http: Quester, uid: string) {
@@ -27,13 +28,17 @@ export async function searchUser(keyword: string, http: Quester) {
   const data = { keyword: keyword, search_type: "bili_user" };
 
   try {
-    const resp = await http.get(BilibiliDynamicType.SearchUserByApp, { params: data }).catch((e) => {
-      if (e.response.status === 412) {
-        return http.get(BilibiliDynamicType.SearchUserByPC, { params: data }).then((resp) => {
-          return resp;
-        });
-      }
-    });
+    const resp = await http
+      .get(BilibiliDynamicType.SearchUserByApp, { params: data })
+      .catch((e) => {
+        if (e.response.status === 412) {
+          return http
+            .get(BilibiliDynamicType.SearchUserByPC, { params: data })
+            .then((resp) => {
+              return resp;
+            });
+        }
+      });
     return resp.data;
   } catch (e) {
     throw new Error("Failed to search user." + e);
@@ -41,9 +46,31 @@ export async function searchUser(keyword: string, http: Quester) {
 }
 
 export async function getMemberCard(http: Quester, uid: string) {
-  
+  try {
+    const resp = await http.get<MemberCard>(
+      StringFormat(BilibiliDynamicType.MemberCardURL, uid)
+    );
+    return resp;
+  } catch (e) {
+    throw new Error("Failed to get member card." + e);
+  }
 }
 
-export async function getMedalWall(http: Quester, uid: string) {
-  
+export async function getMedalWall(http: Quester, uid: string, cookie: string) {
+  try {
+    const resp = await http.get<MedalWall>(
+      StringFormat(BilibiliDynamicType.MedalWallURL, uid), {
+        headers: {
+          Referer: `https://space.bilibili.com/${uid}/medal`,
+          cookie: cookie
+        }
+      }
+    );
+
+    if (resp.code === -101) throw new Error("cookie is invalid");
+
+    return resp;
+  } catch (e) {
+    throw new Error(`Failed to get medal wall [${e.message}]`);
+  }
 }
