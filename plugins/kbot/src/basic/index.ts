ecd76eca54a2e6a7460e0ad056eb3dffa7d11017
Kabuda-czh <634469564@qq.com>
2023-02-10 18:23:54 +0800

    ✨ feat: add some basic commands

--- plugins/kbot/src/basic/index.ts
+++ plugins/kbot/src/basic/index.ts
@@ -2,7 +2,7 @@
  * @Author: Kabuda-czh
  * @Date: 2023-01-29 14:28:53
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-02-08 16:31:01
+ * @LastEditTime: 2023-02-10 15:46:29
  * @FilePath: \KBot-App\plugins\kbot\src\basic\index.ts
  * @Description:
  *
@@ -18,34 +18,71 @@ export const Config: Schema<Config> = Schema.object({
   superAdminQQ: Schema.string().required().description("超级管理员QQ号 (必填)"),
 });
 
-
 export const logger = new Logger("KBot-basic");
 
 export const using = ["database"];
 
-export function apply(ctx: Context, config: Config) {
+export async function apply(ctx: Context, config: Config) {
   ctx.on("bot-status-updated", (bot) => {
     if (bot.status === "online") {
-      logger.success("KBot已上线")
-      ctx.database
-        .getUser(bot.platform, config.superAdminQQ)
-        .then((user) => {
-          try {
-            if (user && user?.authority < 5) {
-              ctx.database.setUser(bot.platform, config.superAdminQQ, {
-                authority: 5,
-              });
-              logger.success(`已将QQ号为 ${config.superAdminQQ} 的用户权限设置为 5 级`)
-            } else if (!user) {
-              ctx.database.createUser(bot.platform, config.superAdminQQ, {
-                authority: 5,
-              });
-              logger.success(`已成功创建QQ号为 ${config.superAdminQQ} 的用户, 并赋予权限 5 级`)
-            }
-          } catch (err) {
-            logger.error(`设置QQ号为 ${config.superAdminQQ} 的用户权限时出错: ${err}`)
+      logger.success("KBot已上线");
+      ctx.database.getUser(bot.platform, config.superAdminQQ).then((user) => {
+        try {
+          if (user && user?.authority < 5) {
+            ctx.database.setUser(bot.platform, config.superAdminQQ, {
+              authority: 5,
+            });
+            logger.success(
+              `已将QQ号为 ${config.superAdminQQ} 的用户权限设置为 5 级`
+            );
+          } else if (!user) {
+            ctx.database.createUser(bot.platform, config.superAdminQQ, {
+              authority: 5,
+            });
+            logger.success(
+              `已成功创建QQ号为 ${config.superAdminQQ} 的用户, 并赋予权限 5 级`
+            );
           }
-        });
+        } catch (err) {
+          logger.error(
+            `设置QQ号为 ${config.superAdminQQ} 的用户权限时出错: ${err}`
+          );
+        }
+      });
     }
   });
+
+  ctx
+    .command("查询天气 <city:string>", "查询城市天气")
+    .action(async ({ session }, city) => {
+      const weather = await ctx.http
+        .get(`https://api.pmay.cn/api/qqtq?msg=${city}`)
+        .catch((err) => {
+          logger.error(`获取天气信息时出错: ${err}`);
+        });
+      if (!weather) return "获取天气信息失败";
+      return weather;
+    });
+
+  ctx.command("一言", "随机一言").action(async ({ session }) => {
+    const yiyan = await ctx.http
+      .get("https://api.pmay.cn/api/yiyan")
+      .catch((err) => {
+        logger.error(`获取一言时出错: ${err}`);
+      });
+
+    if (!yiyan) return "获取一言失败";
+    return yiyan;
+  })
+
+  ctx.command("人间", "随机一言散文集《我在人间凑数的日子》").action(async ({ session }) => {
+    const {data: {yiyan: renjian}} = await ctx.http
+      .get("https://api.pmay.cn/api/renjian")
+      .catch((err) => {
+        logger.error(`获取人间一言时出错: ${err}`);
+      });
+
+    if (!renjian) return "获取人间一言失败";
+    return renjian;
+  })
 }
