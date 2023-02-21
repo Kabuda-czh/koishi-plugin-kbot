14115cb13dc1f358554e0b0a2baea668bca988fd
Kabuda-czh <634469564@qq.com>
2023-02-21 17:29:11 +0800

    🦄 refactor: review code & change weather api

--- plugins/kbot/src/basic/index.tsx
+++ plugins/kbot/src/basic/index.tsx
@@ -2,7 +2,7 @@
  * @Author: Kabuda-czh
  * @Date: 2023-01-29 14:28:53
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-02-16 11:05:59
+ * @LastEditTime: 2023-02-21 17:28:32
  * @FilePath: \KBot-App\plugins\kbot\src\basic\index.tsx
  * @Description:
  *
@@ -13,10 +13,12 @@ import * as statusPlugin from "./status";
 
 export interface Config {
   alApiToken?: string;
+  body?: boolean;
 }
 
 export const Config: Schema<Config> = Schema.object({
   alApiToken: Schema.string().description("ALAPI Token, 注: 前往 https://www.alapi.cn/ 个人中心获取token (可选)"),
+  body: Schema.boolean().default(false).description("是否开启自带的 status, 等同于插件 `status-pro`"),
 });
 
 export const logger = new Logger("KBot-basic");
@@ -24,22 +26,10 @@ export const logger = new Logger("KBot-basic");
 export const using = ["database"];
 
 export async function apply(ctx: Context, config: Config) {
-  ctx.plugin(statusPlugin)
-
-  ctx
-    .command("kbot/天气 <city:string>", "查询城市天气")
-    .shortcut(/^查询(.+)天气$/, { args: ['$1'] })
-    .action(async ({ session }, city) => {
-      const weather = await ctx.http
-        .get(`https://api.pmay.cn/api/qqtq?msg=${city}`)
-        .catch((err) => {
-          logger.error(`获取天气信息时出错: ${err}`);
-        });
-      if (!weather) return "获取天气信息失败";
-      return weather;
-    });
-
-  ctx.command("kbot/一言", "随机一言").action(async ({ session }) => {
+  ctx.command("kbot/一言", "随机一言", {
+    checkArgCount: true,
+    showWarning: false
+  }).action(async ({ session }) => {
     const yiyan = await ctx.http
       .get("https://api.pmay.cn/api/yiyan")
       .catch((err) => {
@@ -51,7 +41,10 @@ export async function apply(ctx: Context, config: Config) {
     return yiyan;
   })
 
-  ctx.command("kbot/人间", "随机一言散文集《我在人间凑数的日子》").action(async ({ session }) => {
+  ctx.command("kbot/人间", "随机一言散文集《我在人间凑数的日子》", {
+    checkArgCount: true,
+    showWarning: false
+  }).action(async ({ session }) => {
     const data = await ctx.http
       .get("https://api.pmay.cn/api/renjian")
       .catch((err) => {
@@ -63,7 +56,10 @@ export async function apply(ctx: Context, config: Config) {
   })
 
   if (config.alApiToken) {
-    ctx.command("kbot/今日新闻", "获取60秒看世界新闻").action(async ({ session }) => {
+    ctx.command("kbot/今日新闻", "获取60秒看世界新闻", {
+      checkArgCount: true,
+      showWarning: false
+    }).action(async ({ session }) => {
       await ctx.http
         .get(`https://v2.alapi.cn/api/zaobao?token=${config.alApiToken}&format=json`)
         .then(res => {
@@ -75,7 +71,31 @@ export async function apply(ctx: Context, config: Config) {
           logger.error(`获取今日新闻时出错: ${err}`);
           return "获取今日新闻失败";
         })
-
     })
+
+    ctx
+      .command("kbot/天气 <city:string>", "查询城市天气")
+      .shortcut(/^查询(.+)天气$/, { args: ['$1'] })
+      .action(async ({ session }, city) => {
+        const weather = await ctx.http
+          .get(`https://v2.alapi.cn/api/tianqi?token=${config.alApiToken}&city=${city}`)
+          .catch((err) => {
+            logger.error(`获取天气信息时出错: ${err}`);
+          });
+        if (!weather) return "获取天气信息失败";
+        const weatherData = weather.data;
+        return `
+城市: ${weatherData.city}
+气温: ${weatherData.min_temp}℃/${weatherData.max_temp}℃ ${weatherData.weather}
+日出时间: ${weatherData.sunrise}
+日落时间: ${weatherData.sunset}
+今日天气实况: 气温: ${weatherData.temp}℃; 风力/风向: ${weatherData.wind} ${weatherData.wind_speed}; 湿度: ${weatherData.humidity}; 空气等级: ${weatherData.aqi.air_level};
+${Object.values(weatherData.index).map((item: any) => `${item.name}: ${item.level}, ${item.content}`).join("\n")}
+`;
+      });
+  }
+
+  if (config.body) {
+    ctx.plugin(statusPlugin)
   }
 }
