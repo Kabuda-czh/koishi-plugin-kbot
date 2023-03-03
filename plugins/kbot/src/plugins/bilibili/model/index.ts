5d032ed039d5f5d3a886fb9cf68df48c92d50a3d
Kabuda-czh <634469564@qq.com>
2023-03-03 14:50:55 +0800

    ✨ feat: add danmuku model

--- plugins/kbot/src/plugins/bilibili/model/index.ts
+++ plugins/kbot/src/plugins/bilibili/model/index.ts
@@ -2,7 +2,7 @@
  * @Author: Kabuda-czh
  * @Date: 2023-01-30 17:18:31
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-01-30 17:21:13
+ * @LastEditTime: 2023-03-03 10:15:35
  * @FilePath: \KBot-App\plugins\kbot\src\plugins\bilibili\model\index.ts
  * @Description: 
  * 
@@ -10,4 +10,5 @@
  */
 export * from "./dynamic.model";
 export * from "./userInfo.model";
-export * from "./composition.model"
+export * from "./composition.model";
+export * from "./danmuku.model"
