9fd1c7175bbc644dfede4d1c89376402233010f1
Kabuda-czh <634469564@qq.com>
2023-03-10 16:15:21 +0800

    🦄 refactor: review code & use ElTableV2

--- plugins/kbot/client/components/GroupDialog.vue
+++ plugins/kbot/client/components/GroupDialog.vue
@@ -2,7 +2,7 @@
  * @Author: Kabuda-czh
  * @Date: 2023-01-31 16:17:01
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-02-14 12:45:02
+ * @LastEditTime: 2023-03-10 14:29:06
  * @FilePath: \KBot-App\plugins\kbot\client\components\GroupDialog.vue
  * @Description:
  *
@@ -10,8 +10,11 @@
 -->
 <script setup lang="ts">
 import { View } from '@element-plus/icons-vue'
-import { computed, ref } from 'vue'
+import type { Column } from 'element-plus'
+import { ElAutoResizer, ElButton, ElImage, ElTableV2 } from 'element-plus'
+import { computed, h, reactive, ref } from 'vue'
 import { fetchGuildMemberList } from '../api'
+import type { UserInfo } from '../interface'
 import GroupMemberDialog from './GroupMemberDialog.vue'
 import FuzzySearch from './FuzzySearch.vue'
 
@@ -40,15 +43,65 @@ const dialogVisible = computed({
 })
 const dialogLoading = ref<boolean>(false)
 
-interface UserInfo {
-  userId: string
-  username: string
-  avatar?: string
-  nickname: string
-}
+const memberDialogVisible = ref<boolean>(false)
+const memberId = ref<string>('')
 
 const defaultMemberList = ref<UserInfo[]>([])
 const memberList = ref<UserInfo[]>([])
+const columns = reactive<Column<any>[]>([
+  {
+    key: 'userId',
+    dataKey: 'userId',
+    title: 'QQ号',
+    width: 150,
+    align: 'center',
+  },
+  {
+    key: 'username',
+    dataKey: 'username',
+    title: 'QQ名称',
+    width: 350,
+    align: 'center',
+  },
+  {
+    key: 'avatar',
+    dataKey: 'avatar',
+    title: '头像',
+    width: 100,
+    align: 'center',
+    cellRenderer: ({ cellData: avatar }) => {
+      return h(ElImage, {
+        src: avatar,
+        fit: 'cover',
+        style: {
+          width: '30px',
+          height: '30px',
+          borderRadius: '50%',
+        },
+      })
+    },
+  },
+  {
+    key: 'operation',
+    title: '查看群员详细信息',
+    width: 350,
+    align: 'center',
+    cellRenderer: ({ rowData: { userId } }) => {
+      return h(
+        ElButton,
+        {
+          circle: true,
+          size: 'large',
+          icon: View,
+          onClick: (e) => {
+            memberId.value = userId
+            memberDialogVisible.value = true
+          },
+        },
+      )
+    },
+  },
+])
 
 const getMemberList = async () => {
   if (props.groupId) {
@@ -59,9 +112,6 @@ const getMemberList = async () => {
     })
   }
 }
-
-const memberDialogVisible = ref<boolean>(false)
-const memberId = ref<string>('')
 
 const selectData = (item: { label: string; value: string | number }) => {
   dialogLoading.value = true
@@ -88,67 +138,42 @@ const dataChange = (value: string | number) => {
 
 <template>
   <el-dialog
-    v-model="dialogVisible"
-    width="50%"
-    title="群成员列表"
-    destroy-on-close
-    @open="getMemberList"
+    v-model="dialogVisible" width="50%" title="群成员列表" destroy-on-close @open="getMemberList"
     @closed="dialogVisible = false"
   >
-    <div class="memberSearch">
-      <p>搜索群成员</p>
-      <FuzzySearch
-        :options="defaultMemberList"
-        label-key="username"
-        value-key="userId"
-        @select-data="selectData"
-        @data-change="dataChange"
-      />
+    <div class="dialogContainer">
+      <div class="memberSearch">
+        <FuzzySearch
+          :options="defaultMemberList" label-key="username" value-key="userId" @select-data="selectData"
+          @data-change="dataChange"
+        />
+        <p>搜索群成员</p>
+      </div>
+      <ElAutoResizer>
+        <template #default="{ width }">
+          <ElTableV2 v-loading="dialogLoading" :data="memberList" :columns="columns" :height="700" :width="width" :max-height="700" />
+        </template>
+      </ElAutoResizer>
     </div>
-    <el-table v-loading="dialogLoading" :data="memberList" max-height="70vh">
-      <el-table-column align="center" prop="userId" label="QQ号" width="150" />
-      <el-table-column align="center" prop="username" label="QQ名称" />
-      <el-table-column align="center" label="头像" width="80">
-        <template #default="{ row }">
-          <el-image
-            style="width: 30px; height: 30px; border-radius: 50%"
-            :src="row.avatar"
-            fit="fill"
-          />
-        </template>
-      </el-table-column>
-      <el-table-column align="center" label="查看群员详细信息">
-        <template #default="{ row }">
-          <el-button
-            :icon="View"
-            size="large"
-            circle
-            @click="
-              memberDialogVisible = true;
-              memberId = row.userId;
-            "
-          />
-        </template>
-      </el-table-column>
-    </el-table>
   </el-dialog>
 
   <GroupMemberDialog
-    :group-id="props.groupId"
-    :visible="memberDialogVisible"
-    :bot-id="props.botId"
-    :bot-role="props.botRole"
-    :member-id="memberId"
-    @closed="memberDialogVisible = false"
+    :group-id="props.groupId" :visible="memberDialogVisible" :bot-id="props.botId"
+    :bot-role="props.botRole" :member-id="memberId" @closed="memberDialogVisible = false"
   />
 </template>
 
 <style scoped>
+.dialogContainer {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
 .memberSearch {
   display: flex;
-  float: right;
+  flex-direction: row-reverse;
   align-items: center;
-  margin-bottom: 10px;
   gap: 15px;
 }
 </style>
