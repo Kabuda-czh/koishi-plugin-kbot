f0fd0abc4cc8beb5d4b636c1187c72ba3fcfd963
Kabuda-czh <634469564@qq.com>
2023-03-10 16:15:43 +0800

    ✨ feat: add group command manage component

--- plugins/kbot/client/components/GroupPlugins.vue
+++ plugins/kbot/client/components/GroupPlugins.vue
@@ -2,34 +2,34 @@
  * @Author: Kabuda-czh
  * @Date: 2023-01-31 16:17:01
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-03-09 17:41:31
+ * @LastEditTime: 2023-03-10 16:12:08
  * @FilePath: \KBot-App\plugins\kbot\client\components\GroupPlugins.vue
  * @Description:
  *
  * Copyright (c) 2023 by Kabuda-czh, All Rights Reserved.
 -->
 <script setup lang="ts">
-import { ElAutoResizer, ElTableV2 } from 'element-plus'
+import { ElMessage } from 'element-plus'
 import { computed, ref } from 'vue'
-import { fetchGuildMemberList } from '../api'
-import GroupMemberDialog from './GroupMemberDialog.vue'
+import { fetchDisabledCommands, fetchSwitchCommands } from '../api'
+import type { GroupCommand } from '../interface'
 import FuzzySearch from './FuzzySearch.vue'
 
 interface Props {
   visible?: boolean
   groupId?: string | number
-  botId?: string | number
-  botRole?: string
+  commands?: GroupCommand[]
 }
 
 const props = withDefaults(defineProps<Props>(), {
   visible: false,
   groupId: 0,
-  botId: 0,
-  botRole: 'member',
+  commands: () => [],
 })
 
 const emits = defineEmits(['closed'])
+
+// dialog
 const dialogVisible = computed({
   get() {
     return props.visible
@@ -40,37 +40,35 @@ const dialogVisible = computed({
 })
 const dialogLoading = ref<boolean>(false)
 
-interface UserInfo {
-  userId: string
-  username: string
-  avatar?: string
-  nickname: string
-}
+// commands
+const commandId = ref<number>(1)
+const commands = ref<GroupCommand[]>([])
+const defaultCommands = ref<GroupCommand[]>([])
 
-const defaultMemberList = ref<UserInfo[]>([])
-const memberList = ref<UserInfo[]>([])
-
-const getMemberList = async () => {
-  if (props.groupId) {
-    dialogLoading.value = true
-
-    defaultMemberList.value = memberList.value = await fetchGuildMemberList(props.groupId).finally(() => {
-      setTimeout(() => (dialogLoading.value = false), 500)
-    })
+const getCommandList = async () => {
+  const setDisable = (commands: GroupCommand[], disables: string[]) => {
+    for (const command of commands) {
+      command.id = commandId.value++
+      command.disable = disables.includes(command.name)
+      setDisable(command.children, disables)
+    }
   }
+
+  await fetchDisabledCommands(props.groupId).then((res: string[]) => {
+    const sliceCommands = props.commands.slice()
+    setDisable(sliceCommands, res)
+    defaultCommands.value = commands.value = sliceCommands
+  })
 }
 
-const memberDialogVisible = ref<boolean>(false)
-const memberId = ref<string>('')
-
-const selectData = (item: { label: string; value: string | number }) => {
+const selectData = (item: { label: string; value: string }) => {
   dialogLoading.value = true
-  const filterList = defaultMemberList.value.filter(
-    member => +member.userId === +item.value,
+  const filterList = defaultCommands.value.filter(
+    commands => commands.name === item.value,
   )
 
   setTimeout(() => {
-    memberList.value = filterList
+    commands.value = filterList
     dialogLoading.value = false
   }, 1000)
 }
@@ -79,55 +77,144 @@ const dataChange = (value: string | number) => {
   if (!value) {
     dialogLoading.value = true
     setTimeout(() => {
-      memberList.value = defaultMemberList.value
+      commands.value = defaultCommands.value
       dialogLoading.value = false
     }, 1000)
   }
 }
+
+const findParentCommand = (command: GroupCommand, commands: GroupCommand[]): GroupCommand => {
+  const parentName = command.parent
+  if (!parentName)
+    return null
+
+  for (const cmd of commands) {
+    if (cmd.name === parentName)
+      return cmd
+
+    if (cmd.children) {
+      const parent = findParentCommand(command, cmd.children)
+      if (parent)
+        return parent
+    }
+  }
+
+  return null
+}
+
+const commandSwitch = (command: GroupCommand) => {
+  if (command.hasChildren)
+    command.children.map(child => child.disable = command.disable)
+  if (command.parent) {
+    const parent = findParentCommand(command, commands.value)
+    let disabled = 0
+    parent.children.forEach(child => child.disable && disabled++)
+    parent.disable = disabled === parent.children.length
+  }
+}
+
+const getAllDisable = (commands: GroupCommand[]): string[] => {
+  const disables: string[] = []
+
+  for (const cmd of commands) {
+    if (cmd.disable)
+      disables.push(cmd.name)
+
+    if (cmd.children.length)
+      disables.push(...getAllDisable(cmd.children))
+  }
+
+  return disables
+}
+
+const setCommands = async () => {
+  const disabledCommands = getAllDisable(commands.value)
+  await fetchSwitchCommands(String(props.groupId), disabledCommands).then((res: boolean) => {
+    if (res)
+      ElMessage.success('设置成功')
+
+    else
+      ElMessage.error('设置失败')
+  })
+}
+
+const init = async () => {
+  dialogLoading.value = true
+  await getCommandList()
+  dialogLoading.value = false
+}
+
+init()
 </script>
 
 <template>
   <el-dialog
-    v-model="dialogVisible"
-    width="50%"
-    title="群插件管理"
-    destroy-on-close
-    @open="getMemberList"
+    v-model="dialogVisible" width="30%" title="群指令管理" destroy-on-close @open="getCommandList"
     @closed="dialogVisible = false"
   >
-    <div class="memberSearch">
-      <p>搜索群成员</p>
-      <FuzzySearch
-        :options="defaultMemberList"
-        label-key="username"
-        value-key="userId"
-        @select-data="selectData"
-        @data-change="dataChange"
-      />
+    <div v-loading="dialogLoading" class="dialogContainer">
+      <div class="commandSearch">
+        <FuzzySearch
+          :options="defaultCommands" label-key="name" value-key="name" @select-data="selectData"
+          @data-change="dataChange"
+        />
+        <p>搜索指令</p>
+      </div>
+      <!-- <ElTable :data="commands" max-height="60vh" style="width: 95vw" row-key="id">
+        <ElTableColumn align="center" label="指令名称" prop="name" />
+        <ElTableColumn align="center" label="是否禁用">
+          <template #default="{ row }">
+            <ElSwitch v-model="row.disable" />
+          </template>
+        </ElTableColumn>
+      </ElTable> -->
+      <el-scrollbar height="40vh">
+        <el-tree
+          :data="commands"
+          node-key="id"
+          :expand-on-click-node="false"
+        >
+          <template #default="{ data }">
+            <div class="custom-tree-node">
+              <span>{{ data.name }}</span>
+              <ElSwitch
+                v-model="data.disable"
+                style="--el-switch-on-color: #ff4949; --el-switch-off-color: #13ce66"
+                @change="commandSwitch(data)"
+              />
+            </div>
+          </template>
+        </el-tree>
+      </el-scrollbar>
     </div>
-    <ElAutoResizer>
-      <template #default="{ height, width }">
-        <ElTableV2 v-loading="dialogLoading" :height="height" :width="width" :data="[]" :columns="[]" fixed />
-      </template>
-    </ElAutoResizer>
+    <template #footer>
+      <ElButton type="primary" @click="setCommands">
+        确认设置
+      </ElButton>
+    </template>
   </el-dialog>
-
-  <GroupMemberDialog
-    :group-id="props.groupId"
-    :visible="memberDialogVisible"
-    :bot-id="props.botId"
-    :bot-role="props.botRole"
-    :member-id="memberId"
-    @closed="memberDialogVisible = false"
-  />
 </template>
 
 <style scoped>
-.memberSearch {
+.dialogContainer {
   display: flex;
-  float: right;
+  flex-direction: column;
+  width: 100%;
+}
+
+.commandSearch {
+  display: flex;
+  flex-direction: row-reverse;
   align-items: center;
-  margin-bottom: 10px;
   gap: 15px;
+}
+
+.custom-tree-node {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 14px;
+  padding-right: 8px;
 }
 </style>
