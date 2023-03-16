cdd9591996f41ec6b518838e186891cc9f3274ef
Kabuda-czh <634469564@qq.com>
2023-03-16 23:25:16 +0800

    🦄 refactor: review code & change wait order

--- plugins/kbot/src/basic/status/render/index.ts
+++ plugins/kbot/src/basic/status/render/index.ts
@@ -16,7 +16,6 @@ import type { Page } from 'puppeteer-core'
 import type { SystemInfo } from '../utils'
 import { writeBlobToFile } from '../utils'
 import { logger } from '..'
-import { getFontsList, loadFont } from '../../../plugins/utils'
 
 export async function renderHtml(ctx: Context, systemInfo: SystemInfo) {
   let page: Page
@@ -24,9 +23,9 @@ export async function renderHtml(ctx: Context, systemInfo: SystemInfo) {
     page = await ctx.puppeteer.page()
     await page.setViewport({ width: 1920 * 2, height: 1080 * 2 })
     await page.goto(`file:///${resolve(__dirname, '../neko/template.html')}`)
-    await page.waitForNetworkIdle()
     await page.evaluate(`action(${JSON.stringify(systemInfo)})`)
     const element = await page.$('#background-page')
+    await page.waitForNetworkIdle()
     return (
       segment.image(await element.screenshot({
         encoding: 'binary',
@@ -34,7 +33,8 @@ export async function renderHtml(ctx: Context, systemInfo: SystemInfo) {
     )
   }
   catch (e) {
-    logger.error('状态渲染失败: ', e)
+    console.log(e)
+    logger.error('puppeteer 渲染失败: ', e.message)
     return `渲染失败${e.message}`
   }
   finally {
@@ -43,8 +43,6 @@ export async function renderHtml(ctx: Context, systemInfo: SystemInfo) {
 }
 
 export async function renderRandom(ctx: Context, sort: string, systemInfo: SystemInfo['random'], botUid: string) {
-  const fontList = await getFontsList(logger)
-  const [fontFace, fontFamily] = await loadFont(fontList)
   return ctx.http.axios<{ pic: string[] }>({
     method: 'get',
     url: `https://iw233.cn/api.php?sort=${sort}&type=json`,
@@ -62,14 +60,15 @@ export async function renderRandom(ctx: Context, sort: string, systemInfo: Syste
     }).then(async (resp) => {
       const imageBase64 = Buffer.from(resp.data, 'binary').toString('base64')
       writeBlobToFile(resp.data, new Date().toLocaleString().replace(/[/:]/g, '-'))
+      const botQQ = botUid.split(':')[1]
       let page: Page
       try {
         page = await ctx.puppeteer.page()
         await page.setViewport({ width: 1920 * 2, height: 1080 * 2 })
         await page.goto(`file:///${resolve(__dirname, '../random/template.html')}`)
-        await page.waitForNetworkIdle()
-        await page.evaluate(`action(${JSON.stringify(systemInfo)}, '${imageBase64}', '${botUid.split(':')[1]}')`)
+        await page.evaluate(`action(${JSON.stringify(systemInfo)}, '${imageBase64}', '${botQQ}')`)
         const element = await page.$('#app')
+        await page.waitForNetworkIdle()
         return (
           segment.image(await element.screenshot({
             encoding: 'binary',
@@ -86,6 +85,8 @@ export async function renderRandom(ctx: Context, sort: string, systemInfo: Syste
     })
   }).catch((err) => {
     logger.error(err.code, err.message)
+    if (['ECONNRESET', 'ETIMEDOUT', 'ECONNREFUSED', 'ECONNABORTED', 'read ECONNRESET'].includes(err.code))
+      return '渲染失败: 请求超时, 网络错误'
     return `渲染失败: ${err.message}`
   })
 }
