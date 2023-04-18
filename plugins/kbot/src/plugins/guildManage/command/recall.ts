0fc819909ac9b78fb6e832c1a9cb78a76c0059b3
Kabuda-czh <634469564@qq.com>
2023-04-18 15:22:43 +0800

    ✨ feat: guildmanage.recall botself

--- plugins/kbot/src/plugins/guildManage/command/recall.ts
+++ plugins/kbot/src/plugins/guildManage/command/recall.ts
@@ -2,23 +2,32 @@
  * @Author: Kabuda-czh
  * @Date: 2023-04-17 11:20:08
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-04-17 14:25:48
+ * @LastEditTime: 2023-04-18 15:20:04
  * @FilePath: \KBot-App\plugins\kbot\src\plugins\guildManage\command\recall.ts
  * @Description:
  *
  * Copyright (c) 2023 by Kabuda-czh, All Rights Reserved.
  */
-import type { Context } from 'koishi'
-import { pick } from 'koishi'
+import type { Context, Dict } from 'koishi'
+import { Time, pick, remove, sleep } from 'koishi'
 import { RoleNumber } from './enum'
 import { getGroupMemberRole } from './util'
 
 export async function initRecall(context: Context) {
+  const recent: Dict<string[]> = {}
+
+  context.guild().on('send', (session) => {
+    const list = recent[session.channelId] ||= []
+    list.unshift(session.messageId)
+    context.setTimeout(() => remove(list, session.messageId), Time.hour)
+  })
+
   context
+    .guild()
     .command('kbot/guildManage.recall <user:user>', '批量撤回成员信息 默认1条',
       {
         checkArgCount: true,
-        authority: 4,
+        authority: 3,
       })
     .alias('批量撤回')
     .option('count', '-c <count:number> 指定撤回条数, 需要大于等于1', { fallback: 1, type: /^[1-9]\d*$/ })
@@ -30,6 +39,7 @@ export async function initRecall(context: Context) {
       const userId = user.split(':')[1]
       const botRole = await getGroupMemberRole(session.bot, session.guildId, session.selfId)
       const qqRole = await getGroupMemberRole(session.bot, session.guildId, userId)
+      const delay = context.root.config?.delay?.broadcast || 500
       if (RoleNumber[botRole] > RoleNumber[qqRole]) {
         const messageList = await context.database
           .select('chat.message')
@@ -48,7 +58,22 @@ export async function initRecall(context: Context) {
         messageList.filter(message => message.deleted !== 1)
           .map(async (message) => {
             await session.bot.internal.deleteMsg(message.messageId)
+            if (delay)
+              await sleep(delay)
           })
+      }
+      else if (userId === session.selfId) {
+        const list = recent[session.channelId]
+        if (!list)
+          return '机器人近期没有发送消息'
+        const removal = list.splice(0, count)
+        if (!list.length)
+          delete recent[session.channelId]
+        for (let index = 0; index < removal.length; index++) {
+          if (index && delay)
+            await sleep(delay)
+          await session.bot.internal.deleteMsg(removal[index])
+        }
       }
       else { return '机器人权限不足' }
     })
