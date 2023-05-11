be22a26210e5255f7e1ef1c4711e2a66c3709e59
Kabuda-czh <634469564@qq.com>
2023-05-11 11:09:42 +0800

    🦄 refactor: delete messages

--- plugins/kbot/src/plugins/guildManage/command/recall.ts
+++ plugins/kbot/src/plugins/guildManage/command/recall.ts
@@ -2,14 +2,15 @@
  * @Author: Kabuda-czh
  * @Date: 2023-04-17 11:20:08
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-04-18 15:20:04
+ * @LastEditTime: 2023-05-11 11:07:55
  * @FilePath: \KBot-App\plugins\kbot\src\plugins\guildManage\command\recall.ts
  * @Description:
  *
  * Copyright (c) 2023 by Kabuda-czh, All Rights Reserved.
  */
 import type { Context, Dict } from 'koishi'
-import { Time, pick, remove, sleep } from 'koishi'
+import { Time, remove, sleep } from 'koishi'
+import type { Message } from '../typings'
 import { RoleNumber } from './enum'
 import { getGroupMemberRole } from './util'
 
@@ -24,7 +25,7 @@ export async function initRecall(context: Context) {
 
   context
     .guild()
-    .command('kbot/guildManage.recall <user:user>', '批量撤回成员信息 默认1条',
+    .command('kbot/guildManage.recall <user:string>', '在 1 天时间内批量撤回成员信息 默认1条',
       {
         checkArgCount: true,
         authority: 3,
@@ -32,37 +33,42 @@ export async function initRecall(context: Context) {
     .alias('批量撤回')
     .option('count', '-c <count:number> 指定撤回条数, 需要大于等于1', { fallback: 1, type: /^[1-9]\d*$/ })
     .usage('注意: 机器人权限至少为管理员且无法撤回管理员消息')
-    .example('guildManage.recall @qq -c 2')
-    .example('批量撤回 @qq')
+    .example('guildManage.recall qq -c 2')
+    .example('批量撤回 qq')
     .action(async ({ session, options }, user) => {
       const count = Math.max(+options.count, 1)
-      const userId = user.split(':')[1]
       const botRole = await getGroupMemberRole(session.bot, session.guildId, session.selfId)
-      const qqRole = await getGroupMemberRole(session.bot, session.guildId, userId)
+      const qqRole = await getGroupMemberRole(session.bot, session.guildId, user)
       const delay = context.root.config?.delay?.broadcast || 500
       if (RoleNumber[botRole] > RoleNumber[qqRole]) {
-        const messageList = await context.database
-          .select('chat.message')
-          .where(
-            pick({
-              platform: session.platform,
-              guildId: session.guildId,
-              channelId: session.channelId,
-              userId,
-            }),
-          )
-          .orderBy('id', 'desc')
-          .limit(count)
-          .execute()
+        const messageList: Message[] = []
 
-        messageList.filter(message => message.deleted !== 1)
-          .map(async (message) => {
-            await session.bot.internal.deleteMsg(message.messageId)
-            if (delay)
-              await sleep(delay)
-          })
+        const getHistoryMsg = async (length: number, messageId?: number) => {
+          const history: { messages: Message[] } = await session.bot.internal.getGroupMsgHistory(session.channelId, messageId)
+          if (!history.messages.length)
+            return
+          const msg = history.messages.filter(message => message.user_id === +user)
+          if (msg.length)
+            messageList.push(...msg.slice(0, length))
+          messageList.sort((a, b) => b.time - a.time)
+          return history.messages[0].time * 1000
+        }
+
+        while (messageList.length < count) {
+          const lastTime = await getHistoryMsg(count - messageList.length, messageList[messageList.length - 1]?.message_id)
+          if (!lastTime)
+            break
+          if (lastTime > Date.now() - Time.day)
+            break
+        }
+
+        messageList.map(async (message) => {
+          await session.bot.internal.deleteMsg(message.message_id)
+          if (delay)
+            await sleep(delay)
+        })
       }
-      else if (userId === session.selfId) {
+      else if (user === session.selfId) {
         const list = recent[session.channelId]
         if (!list)
           return '机器人近期没有发送消息'
