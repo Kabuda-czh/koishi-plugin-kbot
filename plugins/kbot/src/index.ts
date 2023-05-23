294308d8323dcf9f2b3dfdc2739126bd34ef7869
Kabuda-czh <634469564@qq.com>
2023-05-23 11:22:52 +0800

    🐳 chore: update v1.0.6 version

--- plugins/kbot/src/index.ts
+++ plugins/kbot/src/index.ts
@@ -2,7 +2,7 @@
  * @Author: Kabuda-czh
  * @Date: 2023-01-29 14:28:53
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-05-20 16:05:26
+ * @LastEditTime: 2023-05-23 11:20:49
  * @FilePath: \KBot-App\plugins\kbot\src\index.ts
  * @Description:
  *
@@ -28,21 +28,20 @@ import { kbotDir, publicDir } from './config'
 export const name = 'kbot'
 
 export const usage = `
-# KBot v1.0.5 更新日志
+# KBot v1.0.6 更新日志
 
 ## Note
-- 修复 \`bilibili\` 插件无法搜索用户的问题
-- 修复 \`twitter\` 插件查询用户 403 时并未自动获取 token 的问题
-- 优化 \`guildmanage\` 插件
+- 修复 \`bilibili\` 插件无法添加用户的问题
+- 修复 \`bilibili\` 插件添加和删除用户的错误问题
 
 ## Features
-- 优化 \`guildmanage\` 插件代码 [226676e](https://github.com/Kabuda-czh/koishi-plugin-kbot/commit/226676e6d383df30b211d301f212e96a1b8e5eb2) [5a709ed](https://github.com/Kabuda-czh/koishi-plugin-kbot/commit/5a709ed3f259acd9dd3da8ef3fa31d76be29cbf3) [1601dce](https://github.com/Kabuda-czh/koishi-plugin-kbot/commit/1601dceb9199312777ef8155e4f88df006cb067c)
-- 添加 \`guildmanage\` ~~监控群功能~~ (因暂时需要 \`messages\` 插件功能, 并未开放) [42d6cfe](https://github.com/Kabuda-czh/koishi-plugin-kbot/commit/42d6cfe16a7e0f0314003b423d0f1aef6a1e887e)
+- 优化部分代码
 
 ## Bug Fix
-- 修复 \`bilibili\` 因手机端接口异常问题导致 \`code 799\` 查询不到用户 [a97be59](https://github.com/Kabuda-czh/koishi-plugin-kbot/commit/a97be595c8c77cca62bde8f99bab44dd70dff1e2)
-- 修复 \`twitter\` 在搜索用户时请求返回 \`403\` 并未自动获取 \`token\` 的问题 [3db24e8](https://github.com/Kabuda-czh/koishi-plugin-kbot/commit/3db24e81557e2f8fbeb0c5d2e30baefc66d3804c)
-- 修复 \`guildmanage\` 选择群时, 翻页还有自动选择的问题 [1be00aa](https://github.com/Kabuda-czh/koishi-plugin-kbot/commit/1be00aaa1e6d3af75b43395918b3ec5bfcb6ee6b)
+- 修复 \`bilibili\` 因接口增加 md5 校验导致 \`code -403\` 问题
+- 修复 \`bilibili\` 插件添加和删除用户因 uid 类型问题, 导致无法正确使用
+
+详细更新日志请查看: [Release](https://github.com/Kabuda-czh/koishi-plugin-kbot/releases/tag/1.0.6)
 `
 
 interface IPluginEnableConfig {
