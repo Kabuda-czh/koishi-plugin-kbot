7c709dd7a33580334435ff07f797e9873032363b
Kabuda-czh <634469564@qq.com>
2023-06-27 19:14:44 +0800

    🦄 refactor: move enum

--- plugins/kbot/src/plugins/guildManage/enum/index.ts
+++ plugins/kbot/src/plugins/guildManage/enum/index.ts
@@ -1,9 +1,9 @@
 /*
  * @Author: Kabuda-czh
- * @Date: 2023-04-17 14:24:56
+ * @Date: 2023-06-27 18:49:56
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-04-17 14:25:35
- * @FilePath: \KBot-App\plugins\kbot\src\plugins\guildManage\command\enum.ts
+ * @LastEditTime: 2023-06-27 18:50:10
+ * @FilePath: \KBot-App\plugins\kbot\src\plugins\guildManage\enum\index.ts
  * @Description:
  *
  * Copyright (c) 2023 by Kabuda-czh, All Rights Reserved.
