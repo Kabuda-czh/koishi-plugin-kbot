dc3b01a012c75ad6ee6e835a1c8f8622436367b2
Kabuda-czh <634469564@qq.com>
2023-07-04 12:55:24 +0800

    ✨ feat: add group-config/addValid/violation comp

---
+++ plugins/kbot/client/components/GroupConfig.vue
@@ -0,0 +1,227 @@
+<!--
+ * @Author: Kabuda-czh
+ * @Date: 2023-01-31 16:17:01
+ * @LastEditors: Kabuda-czh
+ * @LastEditTime: 2023-07-04 12:26:47
+ * @FilePath: \KBot-App\plugins\kbot\client\components\GroupConfig.vue
+ * @Description:
+ *
+ * Copyright (c) 2023 by Kabuda-czh, All Rights Reserved.
+-->
+<script setup lang="ts">
+import type { Ref } from 'vue'
+import { computed, ref } from 'vue'
+import { message, messageBox } from '@koishijs/client'
+import type { GroupCommand } from '../interface'
+import { fetchGroupLeave, fetchSendMessage } from '../api'
+import GroupPlugins from './GroupPlugins.vue'
+import GroupNotice from './GroupNotice.vue'
+import GroupPortrait from './GroupPortrait.vue'
+import GroupViolation from './GroupViolation.vue'
+import GroupAddValid from './GroupAddValid.vue'
+
+interface Props {
+  visible?: boolean
+  botId?: string | number
+  botRole?: string
+  groupId?: string | number
+  groupName?: string
+  commands?: GroupCommand[]
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  visible: false,
+  groupId: 0,
+  groupName: '',
+  commands: () => [],
+  botId: 0,
+  botRole: '',
+})
+
+const emits = defineEmits(['closed'])
+
+// dialog
+const dialogVisible = computed({
+  get() {
+    return props.visible
+  },
+  set(val) {
+    emits('closed', val)
+  },
+})
+
+const pluginDialogVisible = ref<boolean>(false)
+const groupDialogVisible = ref<boolean>(false)
+const noticeDialogVisible = ref<boolean>(false)
+const portraitDialogVisible = ref<boolean>(false)
+const groupViolationVisible = ref<boolean>(false)
+const groupAddValidVisible = ref<boolean>(false)
+
+function createManageFunction(dialog: Ref<boolean>) {
+  return function () {
+    dialog.value = true
+  }
+}
+
+const manageGroupNotice = createManageFunction(noticeDialogVisible)
+const manageGroupPortrait = createManageFunction(portraitDialogVisible)
+const manageGroupPlugins = createManageFunction(groupDialogVisible)
+const manageGroupViolation = createManageFunction(groupViolationVisible)
+const manageGroupAddValid = createManageFunction(groupAddValidVisible)
+
+function sendMessage(groupId: string | number, groupName: string) {
+  messageBox
+    .prompt('请输入要发送的消息', `向群 ${groupName}(${groupId}) 发送消息`, {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      inputType: 'textarea',
+      inputPlaceholder: '请输入要发送的消息',
+      inputPattern: /\S/,
+      inputErrorMessage: '消息不能为空',
+    })
+    .then(({ value }) => {
+      fetchSendMessage(props.botId, groupId, value)
+    })
+    .catch(() => {
+      message.success('已取消发送')
+    })
+}
+
+function groupLeave(groupId: string | number, isOwner: boolean) {
+  messageBox
+    .confirm(
+      `<strong>确定要${isOwner ? '解散群' : '退群'}吗?</strong>
+      <br />
+      <strong style="color: red">该操作无法撤回!</strong>
+      <br />
+      <i style="color: orange">注意: 当前gocq中解散群接口似乎无效</i>
+      `,
+      '提醒',
+      {
+        dangerouslyUseHTMLString: true,
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'error',
+      },
+    )
+    .then(() => {
+      // TODO 解散群似乎没什么效果
+      fetchGroupLeave(props.botId, groupId, isOwner).then(() => {
+        message.success('操作成功')
+        // getGroupList()
+        location.reload()
+      })
+    })
+    .catch(() => {
+      message.success('已取消操作')
+    })
+}
+</script>
+
+<template>
+  <ElDialog
+    v-model="dialogVisible" width="70%" title="群管理" destroy-on-close
+    @closed="dialogVisible = false"
+  >
+    <ElForm>
+      <ElRow align="middle">
+        <ElCol :span="24">
+          <ElFormItem label="群信息设置">
+            <div style="display: flex; align-items: center; gap: 5px;">
+              <ElButton :disabled="!['owner', 'admin'].includes(props.botRole)" type="primary" @click="manageGroupNotice">
+                发送群公告
+              </ElButton>
+              <ElButton :disabled="!['owner', 'admin'].includes(props.botRole)" type="primary" @click="manageGroupPortrait">
+                更改群头像
+              </ElButton>
+            </div>
+          </ElFormItem>
+        </ElCol>
+      </ElRow>
+      <ElRow align="middle">
+        <ElCol :span="24">
+          <ElFormItem label="群管理配置">
+            <div style="display: flex; align-items: center; gap: 5px;">
+              <ElButton type="primary" @click="manageGroupPlugins">
+                配置群指令
+              </ElButton>
+              <ElButton :disabled="!['owner', 'admin'].includes(props.botRole)" type="primary" @click="manageGroupViolation">
+                配置屏蔽词
+              </ElButton>
+              <ElButton :disabled="!['owner', 'admin'].includes(props.botRole)" type="primary" @click="manageGroupAddValid">
+                配置群验证
+              </ElButton>
+              <!-- <ElButton type="primary" @click="setGuildWatch(row)">
+                  {{ row.isWatch ? '监控已开启' : '监控未开启' }}
+                </ElButton> -->
+            </div>
+          </ElFormItem>
+        </ElCol>
+      </ElRow>
+      <ElRow align="middle">
+        <ElCol :span="24">
+          <ElFormItem label="群对应操作">
+            <div style="display: flex; align-items: center; gap: 5px;">
+              <ElButton @click="sendMessage(props.groupId, props.groupName)">
+                发送群消息
+              </ElButton>
+              <ElButton type="danger" @click="groupLeave(props.groupId, props.botRole === 'owner')">
+                {{ props.botRole === "owner" ? "解散群" : "退出群" }}
+              </ElButton>
+            </div>
+          </ElFormItem>
+        </ElCol>
+      </ElRow>
+    </ElForm>
+
+    <GroupPlugins
+      :visible="pluginDialogVisible" :group-id="groupId" :commands="commands" @closed="
+        pluginDialogVisible = false;
+      "
+    />
+
+    <GroupNotice
+      :visible="noticeDialogVisible" :group-id="groupId" :bot-id="botId" @closed="
+        noticeDialogVisible = false;
+      "
+    />
+
+    <GroupPortrait
+      :visible="portraitDialogVisible" :group-id="groupId" :bot-id="botId" @closed="
+        portraitDialogVisible = false;
+      "
+    />
+
+    <GroupViolation
+      :visible="groupViolationVisible" :group-id="groupId" @closed="groupViolationVisible = false;"
+    />
+
+    <GroupAddValid
+      :visible="groupAddValidVisible" :group-id="groupId" @closed="groupAddValidVisible = false;"
+    />
+  </ElDialog>
+</template>
+
+<style scoped>
+.dialogContainer {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.commandSearch {
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  gap: 15px;
+}
+
+.custom-tree-node {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 14px;
+  padding-right: 8px;
+}
+</style>
---
+++ plugins/kbot/client/components/GroupViolation.vue
@@ -0,0 +1,310 @@
+<!--
+ * @Author: Kabuda-czh
+ * @Date: 2023-07-04 10:31:36
+ * @LastEditors: Kabuda-czh
+ * @LastEditTime: 2023-07-04 12:28:56
+ * @FilePath: \KBot-App\plugins\kbot\client\components\GroupViolation.vue
+ * @Description:
+ *
+ * Copyright (c) 2023 by Kabuda-czh, All Rights Reserved.
+-->
+<script setup lang="ts">
+import type { Column } from 'element-plus'
+import { ElAutoResizer, ElButton } from 'element-plus'
+import { computed, h, reactive, ref } from 'vue'
+import { message, messageBox } from '@koishijs/client'
+import { fetchGetViolationList, fetchSetViolationList } from '../api'
+import FuzzySearch from './FuzzySearch.vue'
+import TableBottomButton from './TableBottomButton.vue'
+
+interface Props {
+  visible?: boolean
+  groupId?: string | number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  visible: false,
+  groupId: 0,
+})
+
+const emits = defineEmits(['closed'])
+
+// dialog
+const dialogVisible = computed({
+  get() {
+    return props.visible
+  },
+  set(val) {
+    emits('closed', val)
+  },
+})
+const dialogLoading = ref<boolean>(false)
+
+interface IViolationList {
+  label: string
+  value: string
+}
+
+interface IViolationRes {
+  id: number
+  guildId: string | number
+  handleWay: 'mute' | 'kick'
+  count: number
+  violations: string[]
+}
+
+// violationConfig
+const violationCount = ref<IViolationRes['count']>(3)
+const violationHandleWay = ref<IViolationRes['handleWay']>('mute')
+
+// violationList
+const violationList = ref<IViolationList[]>([])
+const defaultViolationList = ref<IViolationList[]>([])
+
+// ElTableV2 columns
+const columns = reactive<Column<any>[]>([
+  {
+    key: 'index',
+    cellRenderer: ({ rowIndex }) => {
+      return h(
+        'div',
+        {
+          class: 'index',
+        },
+        rowIndex + 1,
+      )
+    },
+    title: '序号',
+    width: 150,
+    align: 'center',
+  },
+  {
+    key: 'value',
+    dataKey: 'value',
+    title: '屏蔽词',
+    width: 350,
+    align: 'center',
+  },
+  {
+    key: 'operation',
+    title: '操作',
+    width: 350,
+    align: 'center',
+    cellRenderer: ({ rowIndex }) => {
+      return h(
+        'div',
+        {
+          class: 'operation',
+        },
+        [
+          h(
+            ElButton,
+            {
+              type: 'primary',
+              size: 'default',
+              onClick: () => {
+                messageBox
+                  .prompt('编辑屏蔽词', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    inputType: 'textarea',
+                    inputPlaceholder: '请输入修改后的屏蔽词',
+                    inputPattern: /\S/,
+                    inputErrorMessage: '屏蔽词不能为空',
+                  })
+                  .then(({ value }) => {
+                    if (value) {
+                      const filterList = defaultViolationList.value.filter(
+                        violation => violation.value === value,
+                      )
+                      if (filterList.length) {
+                        message.error('该屏蔽词已存在')
+                      }
+                      else {
+                        violationList.value.splice(rowIndex, 1, {
+                          label: value.trim(),
+                          value: value.trim(),
+                        })
+                        defaultViolationList.value.splice(rowIndex, 1, {
+                          label: value.trim(),
+                          value: value.trim(),
+                        })
+                      }
+                    }
+                  })
+              },
+            },
+            {
+              default: () => '编辑',
+            },
+          ),
+          h(
+            ElButton,
+            {
+              type: 'danger',
+              size: 'default',
+              onClick: () => {
+                violationList.value.splice(rowIndex, 1)
+                defaultViolationList.value.splice(rowIndex, 1)
+              },
+            },
+            {
+              default: () => '删除',
+            },
+          ),
+        ],
+      )
+    },
+  },
+])
+
+async function getViolationList() {
+  dialogLoading.value = true
+  await fetchGetViolationList(props.groupId).then((res: IViolationRes) => {
+    const violations = res.violations.map((item) => {
+      return {
+        label: item,
+        value: item,
+      }
+    })
+    defaultViolationList.value = violations
+    violationList.value = violations.slice(0)
+
+    violationCount.value = res.count || 3
+    violationHandleWay.value = res.handleWay || 'mute'
+  })
+  dialogLoading.value = false
+}
+
+function selectData(item: { label: string; value: string }) {
+  dialogLoading.value = true
+  const filterList = defaultViolationList.value.filter(
+    violation => violation.value === item.value,
+  )
+
+  setTimeout(() => {
+    violationList.value = filterList
+    dialogLoading.value = false
+  }, 1000)
+}
+
+function dataChange(value: string | number) {
+  if (!value) {
+    dialogLoading.value = true
+    setTimeout(() => {
+      violationList.value = defaultViolationList.value.slice(0)
+      dialogLoading.value = false
+    }, 1000)
+  }
+}
+
+function addViolation() {
+  messageBox
+    .prompt('请输入要添加的屏蔽词', {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      inputType: 'textarea',
+      inputPlaceholder: '请输入要添加的屏蔽词',
+      inputPattern: /\S/,
+      inputErrorMessage: '屏蔽词不能为空',
+    })
+    .then(({ value }) => {
+      if (value) {
+        const filterList = defaultViolationList.value.filter(
+          violation => violation.value === value,
+        )
+        if (filterList.length) {
+          message.error('该屏蔽词已存在')
+        }
+        else {
+          violationList.value.push({
+            label: value.trim(),
+            value: value.trim(),
+          })
+          defaultViolationList.value.push({
+            label: value.trim(),
+            value: value.trim(),
+          })
+        }
+      }
+    })
+}
+
+async function setViolationList() {
+  const violationList = defaultViolationList.value.map(item => item.value)
+  await fetchSetViolationList(props.groupId, violationCount.value, violationHandleWay.value, violationList).then((res) => {
+    if (res.code === 0) {
+      message.success('设置成功')
+      dialogVisible.value = false
+    }
+    else {
+      message.error(`设置失败${res.msg}`)
+    }
+  })
+}
+</script>
+
+<template>
+  <ElDialog
+    v-model="dialogVisible" width="40%" title="群屏蔽词管理" destroy-on-close @open="getViolationList"
+    @closed="dialogVisible = false"
+  >
+    <div v-loading="dialogLoading" class="dialogContainer">
+      <div>
+        <ElForm>
+          <ElRow>
+            <ElCol :span="12">
+              <ElFormItem label="每日违规次数">
+                <ElInputNumber v-model="violationCount" controls-position="right" :controls="false" :precision="0" :min="1" :max="10" />
+              </ElFormItem>
+            </ElCol>
+            <ElCol :span="12">
+              <ElFormItem label="处理方式">
+                <ElSelect v-model="violationHandleWay" placeholder="请选择处理方式">
+                  <ElOption label="禁言" value="mute" />
+                  <ElOption label="踢出" value="kick" />
+                </ElSelect>
+              </ElFormItem>
+            </ElCol>
+          </ElRow>
+          <ElRow>
+            <ElCol :span="24">
+              <ElFormItem label="搜索屏蔽词">
+                <FuzzySearch
+                  :options="defaultViolationList" @select-data="selectData"
+                  @data-change="dataChange"
+                />
+              </ElFormItem>
+            </ElCol>
+          </ElRow>
+        </ElForm>
+      </div>
+      <ElAutoResizer>
+        <template #default="{ width, height }">
+          <ElTableV2 :data="violationList" :columns="columns" :height="600" :width="width" :max-height="height - 80" />
+          <TableBottomButton @add-row="addViolation" />
+        </template>
+      </ElAutoResizer>
+    </div>
+    <template #footer>
+      <ElButton type="primary" @click="setViolationList">
+        确认设置
+      </ElButton>
+    </template>
+  </ElDialog>
+</template>
+
+<style scoped>
+.dialogContainer {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.commandSearch {
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  gap: 15px;
+}
+</style>
