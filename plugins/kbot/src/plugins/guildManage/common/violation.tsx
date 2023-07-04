475158f75bb10391ead8b4ab24199b2ff306f563
Kabuda-czh <634469564@qq.com>
2023-07-04 12:54:46 +0800

    🦄 refactor: review code & add violation valid

--- plugins/kbot/src/plugins/guildManage/common/violation.tsx
+++ plugins/kbot/src/plugins/guildManage/common/violation.tsx
@@ -2,18 +2,25 @@
  * @Author: Kabuda-czh
  * @Date: 2023-06-27 18:51:02
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-06-27 19:18:04
+ * @LastEditTime: 2023-07-04 12:54:26
  * @FilePath: \KBot-App\plugins\kbot\src\plugins\guildManage\common\violation.tsx
  * @Description:
  *
  * Copyright (c) 2023 by Kabuda-czh, All Rights Reserved.
  */
 import type { Context } from 'koishi'
-import type { IConfig } from '..'
 import { RoleNumber } from '../enum'
 import { getGroupMemberRole } from '../utils'
 
 interface IViolationList {
+  id: number
+  guildId: string
+  violations: string[]
+  count: number
+  handleWay: 'mute' | 'kick'
+}
+
+interface IUserViolation {
   id: number
   userId: string
   userCount: number
@@ -22,95 +29,130 @@ interface IViolationList {
 
 declare module 'koishi' {
   interface Tables {
-    'guild.violationList': IViolationList
+    'guildmanage.violationList': IViolationList
+    'guildmanage.userViolation': IUserViolation
   }
 }
 
-export async function initViolation(context: Context, config: IConfig) {
-  context.database.extend('guild.violationList', {
+export async function initViolation(context: Context) {
+  context.database.extend('guildmanage.violationList', {
+    id: 'unsigned',
+    guildId: 'string',
+    violations: {
+      type: 'list',
+      initial: [],
+    },
+    count: {
+      type: 'integer',
+      initial: 3,
+    },
+    handleWay: {
+      type: 'string',
+      initial: 'mute',
+    },
+  }, {
+    autoInc: true,
+  })
+
+  context.database.extend('guildmanage.userViolation', {
     id: 'unsigned',
     userId: 'string',
     userCount: 'integer(3)',
     day: 'integer(2)',
+  }, {
+    autoInc: true,
   })
 
-  if (config.monitorSpeech?.enabled) {
-    context.guild().on('message', async (session) => {
-      const { userId, guildId, content } = session
+  context.guild().on('message', async (session) => {
+    const { userId, guildId, content } = session
 
-      const botRole = await getGroupMemberRole(session.bot, guildId, session.selfId)
-      const userRole = await getGroupMemberRole(session.bot, guildId, userId)
+    const botRole = await getGroupMemberRole(session.bot, guildId, session.selfId)
+    const userRole = await getGroupMemberRole(session.bot, guildId, userId)
 
-      const userRes = await context.database.get('guild.violationList', { userId })
-      const { id, day } = userRes[0]
-      let { userCount } = userRes[0]
-      let isViolation = false
+    let userRes = await context.database.get('guildmanage.userViolation', { userId })
+    const violationRes = await context.database.get('guildmanage.violationList', { guildId })
 
-      if (botRole === 'member' || RoleNumber[userRole] >= RoleNumber[botRole])
-        return
+    if (violationRes.length === 0)
+      return
 
-      const { violations = [], count = 3, handleWay = 'mute' } = config.monitorSpeech
+    if (userRes.length === 0) {
+      await context.database.upsert('guildmanage.userViolation', [{
+        userId,
+        userCount: 0,
+        day: new Date().getDate(),
+      }])
 
-      for (const violation of violations) {
-        if (content.includes(violation)) {
-          if (userRes.length) {
-            if (day === new Date().getDay()) {
-              if (userCount + 1 < count) {
-                await context.database.upsert('guild.violationList', [{
-                  id,
-                  userCount: userCount + 1,
-                }])
-              }
-            }
-            else {
-              await context.database.upsert('guild.violationList', [{
+      userRes = await context.database.get('guildmanage.userViolation', { userId })
+    }
+
+    const { id, day } = userRes[0]
+    let { userCount } = userRes[0]
+    let isViolation = false
+
+    if (botRole === 'member' || RoleNumber[userRole] >= RoleNumber[botRole])
+      return
+
+    const { violations = [], count = 3, handleWay = 'mute' } = violationRes[0]
+
+    for (const violation of violations) {
+      if (content.includes(violation)) {
+        if (userRes.length) {
+          if (day === new Date().getDate()) {
+            if (userCount + 1 < count) {
+              await context.database.upsert('guildmanage.userViolation', [{
                 id,
-                userCount: 1,
-                day: new Date().getDay(),
+                userCount: userCount + 1,
               }])
             }
           }
           else {
-            await context.database.upsert('guild.violationList', [{
-              userId,
+            await context.database.upsert('guildmanage.userViolation', [{
+              id,
               userCount: 1,
-              day: new Date().getDay(),
+              day: new Date().getDate(),
             }])
           }
-          userCount = userCount || 0 + 1
-          isViolation = true
-          break
         }
+        else {
+          await context.database.upsert('guildmanage.userViolation', [{
+            userId,
+            userCount: 1,
+            day: new Date().getDate(),
+          }])
+        }
+        userCount = userCount || 0 + 1
+        isViolation = true
+        break
       }
+    }
 
-      if (isViolation) {
-        await session.bot.deleteMessage(guildId, session.messageId)
-        await session.send(<message>
+    if (isViolation) {
+      await session.bot.deleteMessage(guildId, session.messageId)
+      await session.send(<message>
           <at id={userId} />
-          <p>因为违规发言, 已被撤回</p>
+          <p>因为违规发言, 已被撤回, 今日违规次数 +1, 当前违规次数: {userCount}, 每日违规次数为: {count}</p>
         </message>)
-      }
-      if (userCount < count)
-        return
+    }
+    if (userCount < count)
+      return
 
-      if (handleWay === 'mute') {
-        await session.send(
+    if (handleWay === 'mute') {
+      await session.send(
           <message>
             <at id={userId} />
             <p>因为违规发言, 已被禁言 1 天</p>
           </message>,
-        )
-        await session.bot.muteGuildMember(guildId, userId, 60 * 60 * 24 * 1)
-      }
-      else if (handleWay === 'kick') {
-        await session.send(
+      )
+      await session.bot.muteGuildMember(guildId, userId, 60 * 60 * 24 * 1)
+    }
+    else if (handleWay === 'kick') {
+      await session.send(
           <message>
             <at id={userId} />
             <p>因为违规发言, 已被踢出本群</p>
           </message>,
-        )
-        await session.bot.kickGuildMember(guildId, userId)
-      }
-    })
-  }
+      )
+      await session.bot.kickGuildMember(guildId, userId)
+    }
+  })
 }
