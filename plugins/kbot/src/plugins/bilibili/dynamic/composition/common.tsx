08dac2317584da4856b92567191c5f55ff3ce3c8
Kabuda-czh <634469564@qq.com>
2023-07-24 13:20:27 +0800

    🦄 refactor: delete danmu & vup useImage

--- plugins/kbot/src/plugins/bilibili/dynamic/composition/common.tsx
+++ plugins/kbot/src/plugins/bilibili/dynamic/composition/common.tsx
@@ -2,7 +2,7 @@
  * @Author: Kabuda-czh
  * @Date: 2023-02-06 17:22:33
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-07-11 18:15:29
+ * @LastEditTime: 2023-07-24 13:19:56
  * @FilePath: \KBot-App\plugins\kbot\src\plugins\bilibili\dynamic\composition\common.tsx
  * @Description:
  *
@@ -41,89 +41,85 @@ export async function bilibiliVupCheck(
   const { uid } = up
   try {
     const { bilibiliVupPath, bilibiliCookiePath, renderFontsDir } = GeneratePath.getInstance(ctx.baseDir).getGeneratePathData()
-    if (config.useImage) {
-      if (ctx.puppeteer) {
-        const searchUserCardInfo = await getMemberCard(ctx.http, uid)
 
-        const needLoadFontList = await getFontsList(renderFontsDir, logger)
+    if (ctx.puppeteer) {
+      const searchUserCardInfo = await getMemberCard(ctx.http, uid)
 
-        let vdb, cookie
+      const needLoadFontList = await getFontsList(renderFontsDir, logger)
 
-        try {
-          vdb = JSON.parse(
-            await fs.promises.readFile(
-              bilibiliVupPath,
-              'utf-8',
-            ),
-          )
-        }
-        catch (e) {
-          logger.error(`Failed to get vup info. ${e}`)
-          throw new Error('vtb信息未找到, 请使用 --re 或 --refresh 更新vup信息')
-        }
+      let vdb, cookie
 
-        try {
-          cookie = JSON.parse(
-            await fs.promises.readFile(
-              bilibiliCookiePath,
-              'utf-8',
-            ),
-          )
-        }
-        catch (e) {
-          logger.error(`Failed to get cookie info. ${e}`)
-          throw new Error('cookie信息未找到, 请使用 --ck 或 --cookie 更新cookie信息')
-        }
-
-        let vups: any[] = vdb.filter(vup => searchUserCardInfo.card.attentions.includes(vup.mid))
-
-        const vupsLength = vups.length
-
-        const cookieString = Object.entries(cookie)
-          .map(([key, value]) => `${key}=${value}`)
-          .join('; ')
-
-        const medals = await getMedalWall(ctx.http, uid, cookieString)
-
-        const medalArray = medals.data.list
-
-        medalArray.sort((a, b) => b.medal_info.level - a.medal_info.level)
-
-        const frontVups = []
-        const medalMap = {}
-
-        for (const medal of medalArray) {
-          const up = {
-            mid: medal.medal_info.target_id,
-            uname: medal.target_name,
-          }
-
-          frontVups.push(up)
-          medalMap[medal.medal_info.target_id] = medal
-        }
-
-        vups = frontVups.concat(vups)
-        vups = frontVups.concat(vups.slice(frontVups.length))
-        for (let i = frontVups.length; i < vups.length; i++) {
-          if (medalMap[vups[i].mid]) {
-            vups.splice(i, 1)
-            i--
-          }
-        }
-
-        if (vups.length > 50)
-          await session.send('成分太多了, 只显示前50个')
-
-        const image = await renderVup(searchUserCardInfo, vups, vupsLength, medalMap, needLoadFontList)
-
-        await session.send(image)
+      try {
+        vdb = JSON.parse(
+          await fs.promises.readFile(
+            bilibiliVupPath,
+            'utf-8',
+          ),
+        )
       }
-      else {
-        return '未安装/启用puppeteer, 无法渲染成分信息!'
+      catch (e) {
+        logger.error(`Failed to get vup info. ${e}`)
+        throw new Error('vtb信息未找到, 请使用 --re 或 --refresh 更新vup信息')
       }
+
+      try {
+        cookie = JSON.parse(
+          await fs.promises.readFile(
+            bilibiliCookiePath,
+            'utf-8',
+          ),
+        )
+      }
+      catch (e) {
+        logger.error(`Failed to get cookie info. ${e}`)
+        throw new Error('cookie信息未找到, 请使用 --ck 或 --cookie 更新cookie信息')
+      }
+
+      let vups: any[] = vdb.filter(vup => searchUserCardInfo.card.attentions.includes(vup.mid))
+
+      const vupsLength = vups.length
+
+      const cookieString = Object.entries(cookie)
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ')
+
+      const medals = await getMedalWall(ctx.http, uid, cookieString)
+
+      const medalArray = medals.data.list
+
+      medalArray.sort((a, b) => b.medal_info.level - a.medal_info.level)
+
+      const frontVups = []
+      const medalMap = {}
+
+      for (const medal of medalArray) {
+        const up = {
+          mid: medal.medal_info.target_id,
+          uname: medal.target_name,
+        }
+
+        frontVups.push(up)
+        medalMap[medal.medal_info.target_id] = medal
+      }
+
+      vups = frontVups.concat(vups)
+      vups = frontVups.concat(vups.slice(frontVups.length))
+      for (let i = frontVups.length; i < vups.length; i++) {
+        if (medalMap[vups[i].mid]) {
+          vups.splice(i, 1)
+          i--
+        }
+      }
+
+      if (vups.length > 50)
+        await session.send('成分太多了, 只显示前50个')
+
+      const image = await renderVup(searchUserCardInfo, vups, vupsLength, medalMap, needLoadFontList)
+
+      await session.send(image)
     }
     else {
-      return '该功能需要开启图片模式支持!'
+      return '未安装/启用puppeteer, 无法渲染成分信息!'
     }
   }
   catch (e) {
@@ -148,34 +144,27 @@ export async function bilibiliDanmuCheck(
     ][]
   >,
   ctx: Context,
-  config: IConfig,
+  _config: IConfig,
 ) {
   const { uid } = up
   try {
-    if (config.useImage) {
-      if (ctx.puppeteer) {
-        const { renderFontsDir } = GeneratePath.getInstance(ctx.baseDir).getGeneratePathData()
+    if (ctx.puppeteer) {
+      const { renderFontsDir } = GeneratePath.getInstance(ctx.baseDir).getGeneratePathData()
 
-        const searchUserCardInfo = await getMemberCard(ctx.http, uid)
+      const searchUserCardInfo = await getMemberCard(ctx.http, uid)
 
-        const needLoadFontList = await getFontsList(renderFontsDir, logger)
+      const needLoadFontList = await getFontsList(renderFontsDir, logger)
 
-        let danmuku = await getDanmukuData(ctx.http, BilibiliDynamicType.DanmakuAPI, uid)
+      let danmuku = await getDanmukuData(ctx.http, BilibiliDynamicType.DanmakuAPI, uid)
 
-        if (!(Object.prototype.toString.call(danmuku) === '[object object]'))
-          danmuku = await getDanmukuData(ctx.http, BilibiliDynamicType.DanmakuAPI2, uid)
+      if (!(Object.prototype.toString.call(danmuku) === '[object object]'))
+        danmuku = await getDanmukuData(ctx.http, BilibiliDynamicType.DanmakuAPI2, uid)
 
-        const image = await renderDanmu(searchUserCardInfo, danmuku, needLoadFontList)
+      const image = await renderDanmu(searchUserCardInfo, danmuku, needLoadFontList)
 
-        await session.send(image)
-      }
-      else {
-        return '未安装/启用puppeteer, 无法渲染弹幕信息!'
-      }
+      await session.send(image)
     }
-    else {
-      return '该功能需要开启图片模式支持!'
-    }
+    else { return '未安装/启用puppeteer, 无法渲染弹幕信息!' }
   }
   catch (e) {
     if (['ECONNREFUSED', 'ECONNRESET'].includes(e.code))
